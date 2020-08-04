01c32f38978d61b5e05fbf67ed8c89f8fd396d10
Hexadecane <36948730+Hexadecane@users.noreply.github.com>
2020-08-04 02:02:31 -0400

    Initial Commit.

---
+++ Sapari Server Setter/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Sapari_Server_Setter {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.SetIPButton = new System.Windows.Forms.Button();
+            this.serverNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GameDirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FolderBrowserButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SetIPButton
+            // 
+            this.SetIPButton.AutoSize = true;
+            this.SetIPButton.Location = new System.Drawing.Point(145, 105);
+            this.SetIPButton.Name = "SetIPButton";
+            this.SetIPButton.Size = new System.Drawing.Size(110, 23);
+            this.SetIPButton.TabIndex = 0;
+            this.SetIPButton.Text = "Set Server IP/URL!";
+            this.SetIPButton.UseVisualStyleBackColor = true;
+            this.SetIPButton.Click += new System.EventHandler(this.SetIPButton_Click);
+            // 
+            // serverNameTextBox
+            // 
+            this.serverNameTextBox.Location = new System.Drawing.Point(12, 79);
+            this.serverNameTextBox.Name = "serverNameTextBox";
+            this.serverNameTextBox.Size = new System.Drawing.Size(376, 20);
+            this.serverNameTextBox.TabIndex = 1;
+            this.serverNameTextBox.TextChanged += new System.EventHandler(this.serverNameTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "- Server IP:Port / URL: -";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(400, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // GameDirectoryTextBox
+            // 
+            this.GameDirectoryTextBox.Location = new System.Drawing.Point(12, 40);
+            this.GameDirectoryTextBox.Name = "GameDirectoryTextBox";
+            this.GameDirectoryTextBox.Size = new System.Drawing.Size(295, 20);
+            this.GameDirectoryTextBox.TabIndex = 4;
+            this.GameDirectoryTextBox.TextChanged += new System.EventHandler(this.GameDirectoryTextBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "- Game Directory: -";
+            // 
+            // FolderBrowserButton
+            // 
+            this.FolderBrowserButton.Location = new System.Drawing.Point(313, 38);
+            this.FolderBrowserButton.Name = "FolderBrowserButton";
+            this.FolderBrowserButton.Size = new System.Drawing.Size(75, 23);
+            this.FolderBrowserButton.TabIndex = 6;
+            this.FolderBrowserButton.Text = "Browse";
+            this.FolderBrowserButton.UseVisualStyleBackColor = true;
+            this.FolderBrowserButton.Click += new System.EventHandler(this.FolderBrowserButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 136);
+            this.Controls.Add(this.FolderBrowserButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.GameDirectoryTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.serverNameTextBox);
+            this.Controls.Add(this.SetIPButton);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sapari Community Place Server Setter";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SetIPButton;
+        private System.Windows.Forms.TextBox serverNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TextBox GameDirectoryTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button FolderBrowserButton;
+    }
+}
+
---
+++ Sapari Server Setter/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Sapari_Server_Setter {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+
+        private string serverLink;
+        private string chosenDirectory;
+
+
+        private void ShowMessageBoxNotification(string title, string contents) {
+            MessageBoxButtons b = MessageBoxButtons.OK;
+            MessageBox.Show(contents, title, b);
+        }
+
+
+        private bool IsValidWRLFile(string path) {
+            // Checks if the given .wrl file contains a valid Sony_WorldInfo { ... } entry.
+            string[] lines = File.ReadAllLines(path);
+            foreach (string l in lines) {
+                if (l.Contains("Sony_WorldInfo {")) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        private List<string> GetWRLFiles(string dir) {
+            // Recursively fetches all the relevant .wrl files in the directory and subdirectories.
+            List<string> filesInDirectory = Directory.GetFiles(dir).Where(x => x.EndsWith(".wrl")).ToList();
+            string[] foldersInDirectory = Directory.GetDirectories(dir);
+            if (foldersInDirectory.Length <= 0) {
+                // The base case; No subdirectories. Return the files in the directory.
+                return filesInDirectory;
+            }
+            else {
+                foreach (string d in foldersInDirectory) {
+                    // Iterate over the folders found in the directory
+                    filesInDirectory.AddRange(GetWRLFiles(d));
+                }
+                return filesInDirectory;
+            }
+        }
+
+
+        private bool WriteLinkToFile(string file) {
+            // Write the server IP/web address to the file under the Sony_WorldInfo entry.
+            List<string> lines = File.ReadAllLines(file).ToList();
+
+            string lineToWrite = $"\tcpBureau\t\"{serverLink}\"";
+
+            Regex WLSMatchPattern = new Regex("^\\s*cpBureauWLS\\s*\"vrml\\.sony\\.co\\.jp:.*\"");
+            Regex normalMatchPattern = new Regex("^\\s*cpBureau\\s*\".*\"");
+
+            bool foundRegexMatch = false;
+
+            // Just find and replace all entries of CPBureauWLS and CPBureau with lineToWrite, then do duplicate cleanup afterward...
+            for (int i = 0; i < lines.Count; i++) {
+                if (WLSMatchPattern.Match(lines[i]).Success) {
+                    lines[i] = lineToWrite;
+                    foundRegexMatch = true;
+                }
+                else if (normalMatchPattern.Match(lines[i]).Success) {
+                    lines[i] = lineToWrite;
+                    foundRegexMatch = true;
+                }
+            }
+            if (foundRegexMatch == false) {
+                ShowMessageBoxNotification("Write Error", $"Couldn't find relevant cpBureau or cpBureauWLS entry in file {file}!");
+                return false;
+            }
+
+            bool firstMatchFound = false;
+            for (int i = 0; i < lines.Count; i++) {
+                // Find the indices of the duplicates and mark them to be filtered...
+                if (normalMatchPattern.Match(lines[i]).Success) {
+                    if (firstMatchFound == false) {
+                        firstMatchFound = true;
+                    }
+                    else {
+                        lines[i] = "!!!FilterMe";
+                    }
+                }
+            }
+
+            try {
+                File.WriteAllLines(file, lines.Where(x => x.Contains("!!!FilterMe") == false).ToArray());
+            }
+            catch {
+                ShowMessageBoxNotification("Write Error", $"Error writing to file {file}.\n\n Make sure the file isn't set to read-only or presently in use, and make sure you're running the application as administrator!");
+                return false;
+            }
+            return true;
+        }
+
+
+        private bool IsValidGameDirectory(string dir) {
+            // Check if the chosen directory is a valid game directory...
+            if (dir.EndsWith("Community Place Browser") == false) {
+                return false;
+            }
+            string[] subdirectories = Directory.GetDirectories(dir);
+            bool foundWorldFolder = false;
+            foreach (string sd in subdirectories) {
+                if (sd.EndsWith("world")) {
+                    foundWorldFolder = true;
+                }
+            }
+            return foundWorldFolder;
+        }
+
+
+        private void SetIPButton_Click(object sender, EventArgs e) {
+            string currDir = chosenDirectory;
+            string invalidDirMessage = "Invalid game directory. Folder must be named \"Community Place Browser\" and contain a \"world\" folder within.";
+            if (chosenDirectory == null || currDir.Length <= 0) {
+                ShowMessageBoxNotification("Error", invalidDirMessage);
+                return;
+            }
+            if (currDir.Length > 0 && IsValidGameDirectory(currDir) == false) {
+                ShowMessageBoxNotification("Error", invalidDirMessage);
+                return;
+            }
+            List<string> files = GetWRLFiles(currDir).Where(x => IsValidWRLFile(x)).ToList();
+            if (files.Count <= 0) {
+                ShowMessageBoxNotification("Write Error", "Couldn't find any valid .wrl files to write to!");
+                return;
+            }
+            foreach (string f in files) {
+                bool successfulWrite = WriteLinkToFile(f);
+                if (!successfulWrite) {
+                    ShowMessageBoxNotification("Warning", "Couldn't write to all files.");
+                    return;
+                }
+            }
+            ShowMessageBoxNotification("Success", "Server set successfully!");
+        }
+
+
+        private void serverNameTextBox_TextChanged(object sender, EventArgs e) {
+            serverLink = serverNameTextBox.Text;
+        }
+
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e) {
+            string s = "Sapari Community Place Server Setter 1.0\nCopyright © 2020\nLicensed under the MIT license.\nCreated by Hxdce";
+            ShowMessageBoxNotification("About", s);
+        }
+
+
+        private void GameDirectoryTextBox_TextChanged(object sender, EventArgs e) {
+            chosenDirectory = GameDirectoryTextBox.Text;
+        }
+
+
+        private void FolderBrowserButton_Click(object sender, EventArgs e) {
+            FolderBrowserDialog d1 = new FolderBrowserDialog();
+            DialogResult r = d1.ShowDialog();
+            if (r == DialogResult.OK) {
+                chosenDirectory = d1.SelectedPath;
+                GameDirectoryTextBox.Text = d1.SelectedPath;
+            }
+        }
+    }
+}
